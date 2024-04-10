891a785120f37d1230401e1f976111128199e01b
Roseline Twagiramariya <157075654+roselinetwagiramariya@users.noreply.github.com>
2024-04-10 00:33:21 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,16 @@
-# add your code here
+def roseline_caesar(text, shift=5):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():  
+            shift_amount = shift % 26
+            if char.islower():
+                encrypted_text += chr((ord(char) - ord('a') + shift_amount) % 26 + ord('a'))
+            else:
+                encrypted_text += chr((ord(char) - ord('A') + shift_amount) % 26 + ord('A'))
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+plain_text = input("Enter the text to encrypt: ")
+encrypted_text = roseline_caesar(plain_text)
+print("Encrypted text:", encrypted_text)
